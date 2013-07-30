e3ab7589769edb984b9bd627f5227dc994156df9
Matthew Marcus <matthewmarcus@Matthews-Macbook-Pro.local>
2013-07-30 20:33:46 +0100

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Filmclub::Application.config.secret_key_base = '2832d414d2f7e5a696910cf6466407372022908a8b0cafbabe8a796e29e8606f45c9a22581aca84c8c3a248f9a800de58b3fe3fb6724a50ba02a1b55d320e88b'
